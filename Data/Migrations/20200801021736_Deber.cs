d3f9666a063b3530fc7b1fb6f797479816d5027f
Mateoo27 <amc.beltran@yavirac.edu.ec>
2020-07-31 21:50:18 -0500

    vintage

---
+++ Data/Migrations/20200801021736_Deber.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VintageStuff.Data.Migrations
+{
+    public partial class Deber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    NombreCompleto = table.Column<string>(nullable: true),
+                    Telefono = table.Column<int>(nullable: false),
+                    Correo = table.Column<string>(nullable: true),
+                    ImagenPerfil = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
